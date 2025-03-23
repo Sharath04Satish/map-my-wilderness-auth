57387c2edac31ffe250adc0e87f2e4cde1cc071a
Sharath Satish <sharath04satish@gmail.com>
2025-03-22 18:19:41 -0600

    Initial setup. Configured generate-store methods

---
+++ README.md
@@ -0,0 +1,3 @@
+# Map my Wilderness Auth
+
+This module generates and stores public-private key pairs for signing and validating JWT on client systems.
---
+++ generateStoreKeyPairs.js
@@ -0,0 +1,20 @@
+import { generateKeyPairSync } from "crypto";
+import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { privateKey, publicKey } = generateKeyPairSync(`${process.env.CRYPTO_METHOD}`, {
+    modulusLength: parseInt(`${process.env.MODULUS_LENGTH}`),
+    publicKeyEncoding: {
+        type: `${process.env.PUBLIC_KEY_ENC_TYPE}`,
+        format: `${process.env.PUBLIC_KEY_ENC_FORMAT}`
+    },
+    privateKeyEncoding: {
+        type: `${process.env.PRIVATE_KEY_ENC_TYPE}`,
+        format: `${process.env.PRIVATE_KEY_ENC_FORMAT}`
+    }
+});
+
+fs.writeFileSync(`${process.env.PUBLIC_KEY_PATH}`, privateKey, { mode: 0o600 });
+fs.writeFileSync(`${process.env.PRIVATE_KEY_PATH}`, publicKey);
